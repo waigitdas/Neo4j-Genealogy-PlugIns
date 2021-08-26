6e2c2ea0d0d37422d67fdc5cca6b3dce7a9e8ef4
David A Stumpf, MD, PhD <67023119+waigitdas@users.noreply.github.com>
2021-08-26 13:07:35 -0500

    Update mrca.java

--- mrca.java
+++ mrca.java
@@ -5,33 +5,54 @@
  * Woodstock, IL 60098 USA
  */
 package gen;
-    import org.neo4j.driver.*;
+    import org.neo4j.driver.AuthTokens;
+    import org.neo4j.driver.Driver;
+    import org.neo4j.driver.GraphDatabase;
+    import org.neo4j.driver.Result;
+    import org.neo4j.driver.Session;    
+    import org.neo4j.driver.SessionConfig;
+    import org.neo4j.driver.Transaction;
+    import org.neo4j.driver.TransactionWork;
     import org.neo4j.procedure.Name;
     import org.neo4j.procedure.UserFunction;
-
+    import org.neo4j.procedure.Description;
+    import org.neo4j.procedure.Procedure;
+    import org.neo4j.procedure.Mode;
+ 
     import static org.neo4j.driver.Values.parameters;
-
-
-    public class mrca implements AutoCloseable
-{  
-    private final Driver driver;
-
-    public mrca( )
+  
+    
+    public class mrca {          
+                       
+     
+     
+    @UserFunction
+    @Description("Input 2 RNs and get list of MRCAs")
+    @Procedure(mode=Mode.READ)
+    
+    public String mrca(
+        @Name("db") 
+            String db,
+        @Name("rn1") 
+            Long rn1,
+        @Name("rn2") 
+            Long rn2
+      
+)
     {
+        
+        { 
+        String r =JavaQuery("stumpf");
+       return r;
+            }
+     }
+   
+    public String JavaQuery(String db){
+    
+    String qrystr = "match (p1:Person{RN:1})-[r1:father|mother*0..15]->(mrca:Person)<-[r2:father|mother*0..15]-(p2:Person{RN:238}) return mrca.fullname + ' [' + mrca.RN + '] (' + left(mrca.BD,4) +'-' + left(mrca.DD,4) +')'" ;
+      Driver driver;
         driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "cns105" ) );
-            try ( Session session = driver.session() ) {
-                
-         }
-    }
-
-    @Override
-    public void close() throws Exception
-    {
-        driver.close();
-    }
-
-    public void JavaQuery( final String qrystr,  String db )
-    {
+        Session session = driver.session() ;
         try ( Session java_session = driver.session(SessionConfig.forDatabase(db)) )
         {
             String javasession = java_session.writeTransaction(new TransactionWork<String>()
@@ -53,15 +74,17 @@ package gen;
                 }
             } );
             System.out.println( javasession );
+            return javasession ;
         }
     }
 
-           
-    public static void main( String... args ) throws Exception
+    
+        public static void main( String... args ) throws Exception
     {
-        try ( mrca mrcalist = new mrca() )
+         mrca mrcalist = new mrca() ;
         {
-            mrcalist.JavaQuery( "match (p1:Person{RN:1})-[r1:father|mother*0..15]->(mrca:Person)<-[r2:father|mother*0..15]-(p2:Person{RN:600}) return mrca.fullname + ' [' + mrca.RN + '] (' + left(mrca.BD,4) +'-' + left(mrca.DD,4) +')'","stumpf" );
-        }
-    }
+         String z =  mrcalist.JavaQuery("stumpf");
+        
+        } 
+    }     
 }
