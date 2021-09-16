c9d31d8d8ad9986a3205672db11c6c77b0d41b3d
David A Stumpf, MD, PhD <67023119+waigitdas@users.noreply.github.com>
2021-09-16 02:18:36 -0500

    Update queries_to_excel.java

--- java classes/excelLib/queries_to_excel.java
+++ java classes/excelLib/queries_to_excel.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2021 
+ * David A Stumpf, MD, PhD
+ * Who Am I -- Graphs for Genealogists
+ * Woodstock, IL 60098 USA
  */
 package gen.excelLib;
 
