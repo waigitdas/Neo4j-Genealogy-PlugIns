ec36058609a46df1b127f440d1f2fb13cca6cf0c
David A Stumpf, MD, PhD <67023119+waigitdas@users.noreply.github.com>
2022-05-09 08:43:47 -0500

    v 1.0.3 updates

--- java classes/dna/overlap_shared_segments.java
+++ java classes/dna/overlap_shared_segments.java
@@ -8,6 +8,7 @@ package gen.dna;
 
 import gen.neo4jlib.neo4j_qry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import org.neo4j.procedure.Description;
@@ -35,39 +36,51 @@ public class overlap_shared_segments {
     
     public static void main(String args[]) {
         // TODO code application logic here
+         List<String> s3 = Arrays.asList("David A Stumpf", "Diane Collins", "Paul Breevaart");
+        get_segs(s3,100L);
     }
     
-     public Object get_segs(List names, Long overlap) 
+     public static Object get_segs(List names, Long overlap) 
     {
-        gen.neo4jlib.neo4j_info.neo4j_var();
-        gen.neo4jlib.neo4j_info.neo4j_var_reload();
+//        gen.neo4jlib.neo4j_info.neo4j_var();
+//        gen.neo4jlib.neo4j_info.neo4j_var_reload();
         
         String ar ="";
         List<Object> ls = null;
         String Q = "\"";
+       String cq = "";
        
+       try{
         for (int i = 0; i < names.size(); i++) {
         ar = ar + Q + names.get(i) + Q;
         if (i<names.size()-1) {ar = ar + ", ";}
         }
+       }
+       catch(Exception ex) {return ex.getMessage();}
         
-        String cq = "with [" + ar + "] as fn match (m:DNA_Match)-[r:match_segment]->(s:Segment) where r.p in fn and r.m in fn and r.cm>=7 and r.snp_ct>=500 with m,collect(distinct s.Indx) as sc with collect(sc) as sc unwind sc as x call {with x return x as y} return y";
-                //"with [" + ar + "] as fn match (m:DNA_Match)-[r:match_segment]->(s:Segment) where r.p in fn and r.m in fn and r.cm>=7 and r.snp_ct>=500 with m,collect(distinct s.Indx) as sc with collect(sc) as sc return sc";
         try{
+           cq = "with [" + ar + "] as x match (m:DNA_Match)-[r:match_segment]->(s:Segment) where r.p in x and r.m in x and r.cm>=7 and r.snp_ct>=500 with x,case when r.p< r.m then r.p + ':' + r.m else r.m +':' + r.p end as match_pair,collect(distinct s.Indx) as sc return sc as x";
+ 
+        
         ls = gen.neo4jlib.neo4j_qry.qry_obj_list(cq);
         }
-        catch(Exception e) {return "error here\n\n" + e.getMessage();}
+        catch(Exception e) {return "error here\n\n" + cq + "\n\n"  + e.getMessage();}
         
-        String s = "";
-        String[] cj =null;
-        String[] ck =null;
-        
+//        String s = "";
+//        String[] cj =null;
+//        String[] ck =null;
+//        
         gen.dna.overlapping_segs_in_three_lists gdna = new gen.dna.overlapping_segs_in_three_lists();
 //        List<String> AB = gdna.get_segs((List<String>) ls.get(0), (List<String>) ls.get(1),100L);
 //        List<String> AC = gdna.get_segs((List<String>) ls.get(0), (List<String>) ls.get(2),100L);
 //        List<String> BC = gdna.get_segs((List<String>) ls.get(1), (List<String>) ls.get(2),100L);
 //        List<String> ABAC = gdna.get_segs(AB, AC,100L);
-        List<String> Segs_Final = gdna.get_segs((List<String>) ls.get(0), (List<String>) ls.get(1), (List<String>) ls.get(2), 100L);
+        
+List<String> Segs_Final ;
+try{
+    Segs_Final = gdna.get_segs((List<String>) ls.get(0), (List<String>) ls.get(1), (List<String>) ls.get(2), 100L);
+}
+catch (Exception e) {return "****" + e.getMessage() + "\n\n" + ls;}
         
 //       List<String> ABAC = gdna.get_segs(AB,AC,100L);
 //       List<String> Segs_Final = gdna.get_segs(ABAC,BC,100L);
